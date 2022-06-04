faa677d5b329de0f861a2ec13fb608d96086dbf4
Skykine <20130127@stu.fushanedu.cn>
2022-06-04 14:19:36 +0800

    Site updated: 2022-06-04 14:19:27

---
+++ js/src/custom.js
@@ -0,0 +1,8 @@
+/* �ֲ�����ͼƬ */
+$(function () {
+	$.backstretch([
+		  "/images/background/bg1.jpg",
+		  "/images/background/bg2.jpg",
+		  "/images/background/bg3.jpg"
+	], { duration: 60000, fade: 1500 });
+});
